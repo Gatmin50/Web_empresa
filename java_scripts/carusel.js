3e28e68eef5e4b183b6fb57b3bb128df66b7523f
Gatmin50 <tonilube6@gmail.com>
2024-10-21 22:59:08 +0200

    update carrusel

--- java_scripts/carusel.js
+++ java_scripts/carusel.js
@@ -1,1 +1,19 @@
+let index = 0;
 
+function showSlide(i) {
+    const slides = document.querySelectorAll('.slide');
+    if (i >= slides.length) { index = 0 }
+    if (i < 0) { index = slides.length - 1 }
+    slides.forEach((slide, idx) => {
+        slide.style.transform = `translateX(${-index * 100}%)`;
+    });
+}
+
+document.querySelectorAll('.carousel').forEach(carousel => {
+    carousel.addEventListener('click', () => {
+        index++;
+        showSlide(index);
+    });
+});
+
+showSlide(index);
